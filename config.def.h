1200a860e8bc14178f42f99abeea9401a2037052
Auii <saintansis@gmail.com>
2022-05-17 17:55:12 +0800

    remove comments

--- config.def.h
+++ config.def.h
@@ -17,12 +17,9 @@ static const int swallowfloating =
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
 // static double defaultopacity = 0.93;
-static const char *fonts[] = {
-    "Liberation Mono:size=11",
-    "Twitter Color Emoji:pixelsize=11", "Noto Color Emoji:pizelsize=11"};
-/* static const char *fonts[] = {"Liberation Mono:size=11", */
-/*                               "Twitter Color Emoji:pixelsize=11", */
-/*                               "Noto Color Emoji:pizelsize=11"}; */
+static const char *fonts[] = {"Liberation Mono:size=11",
+                              "Twitter Color Emoji:pixelsize=11",
+                              "Noto Color Emoji:pizelsize=11"};
 static const char dmenufont[] = "Liberation Mono:size = 11";
 
 static char normbgcolor[] = "#222222";
